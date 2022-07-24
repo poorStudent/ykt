d4bd0539166005748ec55ba5c0af7f5f6496a308
PoorStudent <2579988653@qq.com>
2022-07-25 00:31:11 +0800

    ykt--修改

--- app/src/main/java/com/vms/ykt/yktStuWeb/Cqooc/cqoocMain.java
+++ app/src/main/java/com/vms/ykt/yktStuWeb/Cqooc/cqoocMain.java
@@ -251,20 +251,73 @@ public class cqoocMain implements Serializable {
 
     public String getOpenTasks(String courseId, String taskId) {
         //作业已做详情
+
         String resp = "";
         mCqApi.getOpenTasks(courseId, taskId);
         return resp;
     }
 
     public String parseTaskInfo() {
+/*attachment: ""
+chapter: {id: "328574", title: "绪论", status: "1", pubClass: ""}
+comments: null
+content: "<p>移动互联网的发展对网络舆情带来哪些影响？请你用三言两语回答。</p>"
+courseId: "334570518"
+created: 1657378207929
+gradeId: null
+id: "59746"
+isJudged: 2
+isMark: "2"
+isPublish: 1
+judgeEnd: 1660924800000
+lastUpdated: 1657387269928
+markDesc: null
+markNum: "5"
+name: "孟育耀"
+noReview: 0
+ownerId: "547472"
+pubClass: ""
+pubClassTitle: ""
+publishDate: 1660924800000
+score: 10
+status: "1"
+submitEnd: 1660060800000
+title: "移动互联网的发展对网络舆情带来哪些影响？"
+unitId: "328574"*/
         String resp = "";
 
         return resp;
     }
 
-    public String parseTaskAnsw() {
+    public String parseTaskAnsw(String resps) {
+            /*attachment: ""
+classId: ""
+content: "<p><span style=\"color: rgb(102, 102, 102); font-family: 宋体; font-size: 14px; white-space: normal;\">移动互联网的发展对网络舆情带来哪些影响</span></p>"
+courseId: "334570518"
+created: 1658678763873
+id: "343967506"
+isWarning: 1
+judgeNum: 0
+lastUpdated: 1658678763873
+name: "魏海旭"
+ownerId: "1556134"
+remarks: null
+reviewScore: null
+score: null
+status: "2"
+taskId: "59746"
+teacher: null
+userInfo: {username: "137352034060125", name: "魏海旭", headimgurl: null}
+username: "137352034060125"*/
         String resp = "";
+        if (resps == null || !resps.contains("data")) {
+            return resp;
+        }
+        JSONArray vJSONArray = Tool.parseJsonA(resps,"data");
+        for (int i = 0; i < vJSONArray.size(); i++) {
+            String js=vJSONArray.getString(i);
 
+        }
         return resp;
     }
 
