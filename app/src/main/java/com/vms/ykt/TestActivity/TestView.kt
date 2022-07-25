8cbed66df93f50bee15b87a530ab01a00a06793d
PoorStudent <2579988653@qq.com>
2022-07-26 00:42:45 +0800

    ykt--test

--- app/src/main/java/com/vms/ykt/TestActivity/TestView.kt
+++ app/src/main/java/com/vms/ykt/TestActivity/TestView.kt
@@ -8,36 +8,40 @@ import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.View
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
 import com.vms.ykt.R
+import kotlinx.coroutines.*
 
 class TestView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : View(context, attrs, defStyleAttr) {
-    private var pant : Paint = Paint().apply{
-        style=Paint.Style.STROKE;
-        strokeWidth=3.0f;
-        color= ContextCompat.getColor(context,R.color.purple_200);
+) : View(context, attrs, defStyleAttr), LifecycleObserver {
+    private var pant: Paint = Paint().apply {
+        style = Paint.Style.STROKE;
+        strokeWidth = 3.0f;
+        color = ContextCompat.getColor(context, R.color.purple_200);
 
     }
-    private var paintText : Paint = Paint().apply{
-        style=Paint.Style.STROKE;
-        strokeWidth=3.0f;
-        textSize=30f;
-        typeface= Typeface.DEFAULT_BOLD;
-        color= ContextCompat.getColor(context,R.color.purple_200);
+    private var paintText: Paint = Paint().apply {
+        style = Paint.Style.STROKE;
+        strokeWidth = 3.0f;
+        textSize = 30f;
+        typeface = Typeface.DEFAULT_BOLD;
+        color = ContextCompat.getColor(context, R.color.purple_200);
 
     }
-    private var paintCircle : Paint = Paint().apply{
-        style=Paint.Style.STROKE;
-        strokeWidth=3.0f;
-        pathEffect=DashPathEffect(floatArrayOf(10f,10f),0f);
-        color= ContextCompat.getColor(context,R.color.purple_200);
+    private var paintCircle: Paint = Paint().apply {
+        style = Paint.Style.STROKE;
+        strokeWidth = 3.0f;
+        pathEffect = DashPathEffect(floatArrayOf(10f, 10f), 0f);
+        color = ContextCompat.getColor(context, R.color.purple_200);
 
     }
-    private var paintCircleLine : Paint = Paint().apply{
-        style=Paint.Style.STROKE;
-        strokeWidth=3.0f;
-        color= ContextCompat.getColor(context,R.color.teal_200);
+    private var paintCircleLine: Paint = Paint().apply {
+        style = Paint.Style.STROKE;
+        strokeWidth = 3.0f;
+        color = ContextCompat.getColor(context, R.color.teal_200);
 
     }
     var mWidth = 0.1f;
@@ -50,11 +54,11 @@ class TestView @JvmOverloads constructor(
     var sYPoint = 0f;
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        mWidth=w.toFloat();
-        mHeight=h.toFloat();
+        mWidth = w.toFloat();
+        mHeight = h.toFloat();
 
-        cXPoint=mWidth/2f;
-        cYPoint=mHeight/2f;
+        cXPoint = mWidth / 2f;
+        cYPoint = mHeight / 2f;
 
         super.onSizeChanged(w, h, oldw, oldh)
     }
@@ -66,22 +70,49 @@ class TestView @JvmOverloads constructor(
     }
 
 
-    private fun DrawWork(canvas: Canvas?){
+    private fun DrawWork(canvas: Canvas?) {
 
         canvas?.apply {
-            drawColor(ContextCompat.getColor(context,R.color.design_default_color_on_secondary));
-            drawLine( sXPoint,cYPoint,mWidth,cYPoint,pant);
-            drawLine(cXPoint,sYPoint,cXPoint,mHeight,pant);
-            drawLine( sXPoint,cYPoint/2*3f,mWidth,cYPoint/2*3f,pant);
+            drawColor(ContextCompat.getColor(context, R.color.design_default_color_on_secondary));
+            drawLine(sXPoint, cYPoint, mWidth, cYPoint, pant);
+            drawLine(cXPoint, sYPoint, cXPoint, mHeight, pant);
+            drawLine(sXPoint, cYPoint / 2 * 3f, mWidth, cYPoint / 2 * 3f, pant);
 
-            drawRect(cXPoint/2,sYPoint+100f,cXPoint/2*3,sYPoint+200f,pant);
-            drawText("Test.TestView",cXPoint/1.3f,sYPoint+150f,paintText);
+            drawRect(cXPoint / 2, sYPoint + 100f, cXPoint / 2 * 3, sYPoint + 200f, pant);
+            drawText("Test.TestView", cXPoint / 1.3f, sYPoint + 150f, paintText);
 
-            var rsx=cXPoint;
-            var rsy=cYPoint/2*3f;
-            var rs=if (rsx>cYPoint/2f)cYPoint/2-5f else rsx-5f;
-            drawCircle(rsx,rsy,rs,paintCircle);
+            var rsx = cXPoint;
+            var rsy = cYPoint / 2 * 3f;
+            var rs = if (rsx > cYPoint / 2f) cYPoint / 2 - 5f else rsx - 5f;
+            drawCircle(rsx, rsy, rs, paintCircle);
+            drawLine(this, rs);
         }
 
+    }
+
+    private var mRota = 10f;
+    private fun drawLine(canvas: Canvas?, wt: Float) {
+        canvas?.apply {
+            translate(cXPoint, cYPoint / 2 * 3f);
+            rotate(-mRota);
+            drawLine(0f, 0f, wt, 0f, paintCircleLine);
+        };
+
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    fun startRotationLine() {
+       job= CoroutineScope(Dispatchers.Main).launch {
+            while (true) {
+                delay(700);
+                mRota+=5f;
+                invalidate();
+            }
+        }
+    }
+    private var job : Job ?=null;
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    fun stopRotationLine() {
+        job?.cancel();// STOPSHIP: 2022/7/26
     }
 }
